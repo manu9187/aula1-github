537551fe911c59299e9c4677048e19ab4e478b56
Daviny <daviny@vidal.dev.br>
2021-11-16 22:19:15 -0300

    add feature new

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("ola mundo");
 		System.out.println("manu");
+		System.out.println("daviny");
 		
 	}
                         
